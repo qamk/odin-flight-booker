f3cec394277749357cfea24f6fc67db472935047
qamk <qamking@gmail.com>
2021-08-26 21:47:31 +0100

    Add main model associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,6 @@
 class Flight < ApplicationRecord
+  has_many :bookings
+  has_many :passengers
+  belongs_to :departing_airport, class_name: 'Airport'
+  belongs_to :arriving_airport, class_name: 'Airport'
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,5 @@
 class Passenger < ApplicationRecord
+  has_many :bookings
+  has_many :booked_flights, through: :bookings
+
 end
